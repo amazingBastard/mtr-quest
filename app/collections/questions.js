02ebaea290ae5dd3617dd37d0509541d6959ed6a
Damir <damir@withpulp.com>
2015-12-17 21:21:26 -0800

    created collections

---
+++ app/collections/questions.js
@@ -0,0 +1,36 @@
+Questions = new Mongo.Collection('questions');
+
+Questions.allow({
+        insert: () => false,
+    update: () => false,
+    remove: () => false
+});
+
+Questions.deny({
+        insert: () => true,
+    update: () => true,
+    remove: () => true
+});
+
+let QuestionsSchema = new SimpleSchema({
+    'question': {
+        type: String,
+        label: 'A question to be asked'
+    },
+    'owner': {
+        type: String,
+        label: 'The owner id of the question object'
+    },
+    // @TODO: sorting algorithm that sets a question type to user question.
+    'type': {
+        type: String,
+        label: 'The type of question in relation to the response',
+        optional: true
+    },
+    'createdAt': {
+        type: Date,
+        label: 'The date the question object was created'
+    }
+});
+
+Questions.attachSchema(QuestionsSchema);
---
+++ app/collections/responses.js
@@ -0,0 +1,35 @@
+Responses = new Mongo.Collection('responses');
+
+Responses.allow({
+        insert: () => false,
+    update: () => false,
+    remove: () => false
+});
+
+Responses.deny({
+        insert: () => true,
+    update: () => true,
+    remove: () => true
+});
+
+let ResponsesSchema = new SimpleSchema({
+    'response': {
+        type: String,
+        label: 'The response string that is sent to the client'
+    },
+    'owner': {
+        type: String,
+        label: 'The owner id of the response object'
+    },
+    'type': {
+        type: String,
+        label: 'The type of response in relation to the question',
+        optional: true
+    },
+    'createdAt': {
+        type: Date,
+        label: 'The date the response object was created'
+    }
+});
+
+Responses.attachSchema(ResponsesSchema);
