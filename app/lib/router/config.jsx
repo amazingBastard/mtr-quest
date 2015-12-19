9459e9c587ffb7bac903a2465b482ba23da78f81
Damir <damir@withpulp.com>
2015-12-18 23:29:46 -0800

    added router config, created private routes

---
+++ app/lib/router/config.jsx
@@ -0,0 +1,14 @@
+Accounts.onLogin(() => {
+    let currentRoute = FlowRouter.current();
+if (currentRoute && currentRoute.route.group.name === 'public') {
+    FlowRouter.go('root');
+}
+});
+
+if (Meteor.isClient) {
+    Tracker.autorun(() => {
+        if (!Meteor.userId() && FlowRouter.current().route) {
+        FlowRouter.go('login');
+    }
+});
+}
---
+++ app/lib/router/private.jsx
@@ -0,0 +1,23 @@
+const privateRedirect = () => {
+    if (!Meteor.loggingIn() && !Meteor.userId()) {
+        FlowRouter.go('login');
+    }
+};
+
+const pageView = () => {
+    GAnalytics.pageview();
+};
+
+const privateRoutes = FlowRouter.group({
+    name: 'private',
+    triggersEnter: [privateRedirect, pageView]
+});
+
+// @TODO: give this route a name
+
+privateRoutes.route('/confirm', {
+    name: 'confirm',
+    action() {
+    ReactLayout.render(App.Layout, {view: <App.Confirm />});
+}
+});
