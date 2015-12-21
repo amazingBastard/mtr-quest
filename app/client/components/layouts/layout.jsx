24be3603e97114fdf8a75f0c3c92712f1c4be38d
Damir <damir@withpulp.com>
2015-12-20 22:14:37 -0800

    setup layout and components/views

---
+++ app/client/components/layouts/layout.jsx
@@ -0,0 +1,19 @@
+App.Layout = React.createClass({
+    render() {
+        return (
+            <layout className="layout">
+                <header className="header">
+                    <App.Logo />
+                </header>
+
+                <main className="main">
+                    {this.props.view}
+                </main>
+
+                <footer className="footer">
+                    <App.Copyright />
+                </footer>
+            </layout>
+        );
+    }
+});
---
+++ app/client/components/views/public/root.jsx
@@ -0,0 +1,9 @@
+App.Root = React.createClass({
+    render() {
+        return (
+            <view className="animated fadeIn root view">
+                root view
+            </view>
+        );
+    }
+});
