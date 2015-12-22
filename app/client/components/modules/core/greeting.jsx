0bcf741108efaabc40e48d44e18b02972a53a600
Damir <damir@withpulp.com>
2015-12-21 19:50:27 -0800

    added greeting module

---
+++ app/client/components/modules/core/greeting.jsx
@@ -0,0 +1,9 @@
+App.Greeting = React.createClass({
+    render() {
+        return (
+            <module className="greeting module">
+                <p>Hello! What's on your mind?</p>
+            </module>
+        );
+    }
+});
--- app/client/components/views/public/root.jsx
+++ app/client/components/views/public/root.jsx
@@ -2,7 +2,7 @@ App.Root = React.createClass({
     render() {
         return (
             <view className="animated fadeIn root view">
-                root view
+                <App.Greeting />
             </view>
         );
     }
