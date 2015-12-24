e4f158bffe459755630740eafa60d74561ffaccc
Damir <damir@withpulp.com>
2015-12-23 23:44:28 -0800

    sub to greetings in greeting module - wip

--- app/client/components/modules/core/greeting.jsx
+++ app/client/components/modules/core/greeting.jsx
@@ -1,9 +1,25 @@
 App.Greeting = React.createClass({
+    mixins: [ReactMeteorData],
+
+    getMeteorData() {
+        let selector = {type: 'casual'},
+            subscription = Meteor.subscribe('greetings', selector);
+
+        return {
+            isLoading: !subscription.ready(),
+            greeting: Greetings.find({}, {sort: {createdAt: -1}, limit: 1}).fetch()
+        };
+    },
+
     render() {
-        return (
-            <module className="centered greeting module">
-                <p className="greeting">Hello! What's on your mind?</p>
-            </module>
-        );
+        if (this.data.isLoading) {
+            return <App.Loading />;
+        } else {
+            return (
+                <module className="centered greeting module">
+                    <p className="greeting">Hello! What's on your mind?</p>
+                </module>
+            );
+        }
     }
 });
