4b8cdbab963f0d6b61fe36f3280ee7ee2a3135ec
Damir <damir@withpulp.com>
2015-12-25 12:12:22 -0800

    greeting wip

--- app/client/components/modules/core/greeting.jsx
+++ app/client/components/modules/core/greeting.jsx
@@ -1,26 +1,36 @@
 App.Greeting = React.createClass({
-    mixins: [ReactMeteorData],
+    //mixins: [ReactMeteorData],
 
-    getMeteorData() {
-        // @TODO: change selector type depending on user
-        let selector = {type: 'casual'},
-            subscription = Meteor.subscribe('greetings', selector);
-
-        return {
-            isLoading: !subscription.ready(),
-            greeting: Greetings.find({}, {sort: {created: -1}, limit: 1}).fetch()
-        };
+    shouldComponentUpdate() {
+        return true;
     },
 
+    //getMeteorData() {
+    //    // @TODO: change selector type depending on user
+    //    let selector = {type: 'casual'},
+    //        subscription = Meteor.subscribe('greetings', selector);
+    //
+    //    return {
+    //        isLoading: !subscription.ready(),
+    //        greeting: Greetings.find({}, {sort: {created: -1}, limit: 1}).fetch()
+    //    };
+    //},
+
     render() {
-        if (this.data.isLoading) {
-            return <App.Loading />;
-        } else {
-            return (
-                <module className="centered greeting module">
-                    <p className="greeting">Hello! What's on your mind?</p>
-                </module>
-            );
-        }
+        return (
+            <module className="centered greeting module">
+                <p className="greeting">Hello! What's on your mind?</p>
+            </module>
+        );
+
+        //if (this.data.isLoading) {
+        //    return <App.Loading />;
+        //} else {
+        //    return (
+        //        <module className="centered greeting module">
+        //            <p className="greeting">Hello! What's on your mind?</p>
+        //        </module>
+        //    );
+        //}
     }
 });
