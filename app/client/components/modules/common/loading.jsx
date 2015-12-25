4c865baf7bbea770e11f6e9531b0f82add757498
Damir <damir@withpulp.com>
2015-12-25 15:35:54 -0800

    greeting module issues resolved - loading module added

---
+++ app/client/components/modules/common/loading.jsx
@@ -0,0 +1,9 @@
+App.Loading = React.createClass({
+    render() {
+        return (
+            <module className="top fixed centered loading module">
+                <p>loading...</p>
+            </module>
+        );
+    }
+});
--- app/client/components/modules/core/greeting.jsx
+++ app/client/components/modules/core/greeting.jsx
@@ -1,36 +1,30 @@
 App.Greeting = React.createClass({
-    //mixins: [ReactMeteorData],
+    mixins: [ReactMeteorData],
 
     shouldComponentUpdate() {
         return true;
     },
 
-    //getMeteorData() {
-    //    // @TODO: change selector type depending on user
-    //    let selector = {type: 'casual'},
-    //        subscription = Meteor.subscribe('greetings', selector);
-    //
-    //    return {
-    //        isLoading: !subscription.ready(),
-    //        greeting: Greetings.find({}, {sort: {created: -1}, limit: 1}).fetch()
-    //    };
-    //},
+    getMeteorData() {
+        // @TODO: change selector type depending on user
+        let selector = {type: 'casual'},
+            subscription = Meteor.subscribe('greetings', selector);
+
+        return {
+            isLoading: !subscription.ready(),
+            greeting: Greetings.find({}, {sort: {created: -1}, limit: 1}).fetch()
+        };
+    },
 
     render() {
-        return (
-            <module className="centered greeting module">
-                <p className="greeting">Hello! What's on your mind?</p>
-            </module>
-        );
-
-        //if (this.data.isLoading) {
-        //    return <App.Loading />;
-        //} else {
-        //    return (
-        //        <module className="centered greeting module">
-        //            <p className="greeting">Hello! What's on your mind?</p>
-        //        </module>
-        //    );
-        //}
+        if (this.data.isLoading) {
+            return <App.Loading />;
+        } else {
+            return (
+                <module className="centered greeting module">
+                    <p className="greeting">Hello! What's on your mind?</p>
+                </module>
+            );
+        }
     }
 });
