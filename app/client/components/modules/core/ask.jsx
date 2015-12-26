9d665256b0b0b87a85982dd683d4503b31aabf87
Damir <damir@withpulp.com>
2015-12-26 12:58:16 -0800

    added form for ask question

--- app/client/components/modules/core/ask.jsx
+++ app/client/components/modules/core/ask.jsx
@@ -1,8 +1,11 @@
 App.Ask = React.createClass({
     render() {
         return (
-            <module className="ask question module">
-                <input type="text" className="question input" />
+            <module className="ask module">
+                <form className="ask question form">
+                    <input type="text" className="question input"/>
+                    <button type="submit" className="primary centered submit button">Ask</button>
+                </form>
             </module>
         );
     }
