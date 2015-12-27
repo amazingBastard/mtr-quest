e48c817a1d012d05e88b5425288a70c471a2eda1
Damir <damir@withpulp.com>
2015-12-27 13:37:26 -0800

    todo for question form

--- app/client/components/modules/core/ask.jsx
+++ app/client/components/modules/core/ask.jsx
@@ -1,10 +1,11 @@
 App.Ask = React.createClass({
+    // @TODO: question form into component
     render() {
         return (
             <module className="ask module">
                 <form className="ask question form">
                     <div className="input group">
-                        <input type="text" className="question input"/>
+                        <input type="text" className="question input" autofocus />
                         <button type="submit" className="secondary centered submit button">Ask</button>
                     </div>
                 </form>
