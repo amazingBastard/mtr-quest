4bcec4fb03e9d1560c53dc4c67321c8f5262c139
Damir <damir@withpulp.com>
2015-12-29 14:37:41 -0800

    setup submit function for question form

--- app/client/components/modules/elements/question_form.jsx
+++ app/client/components/modules/elements/question_form.jsx
@@ -1,7 +1,11 @@
 App.QuestionForm = React.createClass({
+    handleSubmit(event) {
+        event.preventDefault();
+    },
+
     render() {
         return (
-            <form className="ask question form">
+            <form className="ask question form" onSubmit={this.handleSubmit}>
                 <div className="input group">
                     <input type="text" className="question input" autofocus />
                     <button type="submit" className="secondary centered submit button">Ask</button>
